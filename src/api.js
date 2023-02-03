94efbedeaa5f002131196b2c4171a1225c8144f7
guilhas5 <115358315+guilhas5@users.noreply.github.com>
2023-02-03 17:42:18 +0000

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+	  "X-RapidAPI-Key": 'f64b79de54msh6cd0e3db73d375cp13aad7jsnac28360eb935',
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = '3adbfad18dc91c501df1103a06a1c95b';
